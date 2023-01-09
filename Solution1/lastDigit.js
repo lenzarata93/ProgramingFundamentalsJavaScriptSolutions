9f9913a66e087c7d948fbca814f012d36c53de29
lenzarata93 <lenzarata93@gmail.com>
2023-01-09 17:23:20 +0200

    Geting Last Digit and console it with text
    Geting Last Digit and console it with text

---
+++ Solution1/lastDigit.js
@@ -0,0 +1,18 @@
+function lastDigit (num){
+    let lastNum = num%10;
+
+    switch (lastNum) {
+        case 1: console.log(`one`);break;
+        case 2: console.log(`two`);break;
+        case 3:  console.log(`three`);break;
+        case 4:  console.log(`four`);break;   
+        case 5:  console.log(`five`);break;  
+        case 6:  console.log(`six`);break;
+        case 7: console.log(`seven`);break;
+        case 8: console.log(`eight`);break;
+        case 9: console.log(`nine`);break;
+        case 0: console.log(`zero`);break;
+          
+    }
+
+}lastDigit(4347);
